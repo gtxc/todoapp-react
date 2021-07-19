d845887ee590999502fccfc5d2c913b7facfd8a6
gt <gt@arch.com>
2021-07-19 10:01:34 +0300

    Create function components

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,21 +1,36 @@
 import React, {useState} from 'react'
 
-function TodoForm() {
-const [input, setInput] = useState('')
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        })
+
+        setInput("");
+    };
 
     return (
-        <form className="todo-form">
+        <form className="todo-form" onSubmit={handleSubmit}>
             <input
-                type='text'
-                placeholder='Add a todo'
+                type="text"
+                placeholder="Add a todo"
                 value={input}
-                name='text'
-                className='todo-input'
+                name="text"
+                className="todo-input"
+                onChange={handleChange}
             />
+            <button className="todo-button">Add</button>
         </form>
-
-        
-    )        
+    )
 }
 
 export default TodoForm
